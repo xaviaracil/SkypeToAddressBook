5d3d4fb9e5b5634f7749fb8cab82bc5b6670089c
Xavi Aracil <xaviaracil@me.com>
2010-09-14 13:09:53 +0200

    Added -(XSContact *) contactWithSkypeName:(NSString *) skypeName method. Still have to finished main loop.

--- XSMainWindow.m
+++ XSMainWindow.m
@@ -8,10 +8,13 @@
 
 #import "XSMainWindow.h"
 #import "AppDelegate.h"
+#import "XSContact.h"
 
 @interface XSMainWindow () 
 
 @property (nonatomic, retain) NSDictionary *abDictionary;
+
+-(XSContact *) contactWithSkypeName:(NSString *) skypeName;
 
 @end
 
@@ -56,7 +59,38 @@
 	for (NSString *skypeName in contacts) {
 		// TODO: chicha
 		NSString *uniqueId = [abDictionary valueForKey:@"skypeName"];
+        
+        // fetch contact in Core Data
+        // If it doesn't exits, create it
+        // update AB contact, if so
+        XSContact *xsContact = [self contactWithSkypeName:skypeName];
+        
+        if (!uniqueId) {
+            // skypeName does have and AddressBook contact
+        }
 	}
+    
+    // fetch contacts with skype name different than contacts
+    // delete them
 }
 
+#pragma mark -
+#pragma mark Private Methods
+-(XSContact *) contactWithSkypeName:(NSString *)skypeName {
+    AppDelegate *appDelegate = [[NSApplication sharedApplication] delegate];
+    
+    NSManagedObjectContext *moc = appDelegate.managedObjectContext;
+    NSEntityDescription *entityDescription = [NSEntityDescription entityForName:@"Contact" inManagedObjectContext:moc];
+    NSFetchRequest *request = [[[NSFetchRequest alloc] init] autorelease];
+    [request setEntity:entityDescription];
+    
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"skypeName == %@", skypeName];
+    [request setPredicate:predicate];
+    
+    NSError *error;
+    NSArray *array = [moc executeFetchRequest:request error:&error];
+    return (!array || [array count] == 0) ? nil : [array objectAtIndex:0];
+    
+    // TODO: deal with error
+}
 @end
